5e32bb9451a5b7160263f982af9fe65b685389c2
Unknown <nicholasdlchan60@gmail.com>
2019-02-02 18:40:28 -0500

    pageUrlMatching on product pages

--- SoSChromeExtension/background.js
+++ SoSChromeExtension/background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
   chrome.declarativeContent.onPageChanged.addRules([{
     conditions: [new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: {hostEquals: 'www.amazon.ca',schemes: ['https']},
+      pageUrl: {urlMatches: '.+//www.amazon.ca/.+/dp/.+',schemes: ['https']},
     }),
     new chrome.declarativeContent.PageStateMatcher({
       pageUrl: {hostEquals: 'www.amazon.com', schemes: ['https']},
