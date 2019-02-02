cda015b8ea6fa98089b4560658af8d122fd78f01
Unknown <jeremie.bed20@gmail.com>
2019-02-02 14:52:31 -0500

    set up Flask server

--- scraper.py
+++ scraper.py
@@ -1,1 +1,13 @@
-input("Hello, press enter to quit")
+from flask import Flask
+app = Flask(__name__)
+
+
+
+
+
+@app.route("/")
+def pushData():
+	return "The SOS Flask server is running"
+
+if __name__ == "__main__":
+	app.run()
