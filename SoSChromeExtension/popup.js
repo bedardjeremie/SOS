aca461a25a723b4abc059e45330e7a1459e3022f
Unknown <nicholasdlchan60@gmail.com>
2019-02-02 18:40:28 -0500

    pageUrlMatching on product pages

--- SoSChromeExtension/popup.js
+++ SoSChromeExtension/popup.js
@@ -1,4 +1,3 @@
-let changeColorGreen = document.getElementById('changeColorGreen');
 
 // function onClick(element){
 //   let color = element.target.value;
@@ -16,19 +15,19 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
 
 $( document ).ready(function() {
   let budgetText = $('#budget-text');
-  let message = $('#message');
+  // let message = $('#message');
   chrome.storage.sync.get('budget', function(data){
     let budgetAmt = data.budget.toString();
     budgetText.append(budgetAmt);
   })
 
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function() {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-    }
-  });
+  // chrome.tabs.executeScript(null, {
+  //   file: "getPagesSource.js"
+  // }, function() {
+  //   // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+  //   if (chrome.runtime.lastError) {
+  //     message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+  //   }
+  // });
 
 });
