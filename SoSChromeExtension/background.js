8b3de228eb78035f98ed6c299ed93ceb25c0be81
Unknown <nicholasdlchan60@gmail.com>
2019-02-03 01:54:32 -0500

    Enable on all product pages
    change pageUrlMatch to allow all extension to function on all

--- SoSChromeExtension/background.js
+++ SoSChromeExtension/background.js
@@ -2,10 +2,13 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
   chrome.declarativeContent.onPageChanged.addRules([{
     conditions: [new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: {urlMatches: '.+//www.amazon.ca/.+/dp/.+',schemes: ['https']},
+      pageUrl: {urlMatches: '.+//www.amazon.ca/.+/dp/.+',schemes: ['https']}
     }),
     new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: {hostEquals: '.+//www.amazon.com/.+/dp/.+', schemes: ['https']},
+      pageUrl: {urlMatches: '.+//www.amazon.ca/dp/.+',schemes: ['https']}
+    }),
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: {hostEquals: '.+//www.amazon.com/.+/dp/.+', schemes: ['https']}
     })
     ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
