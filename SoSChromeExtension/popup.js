7ab2ae68e6705036865a0c7bb176d84604750cf5
Unknown <nicholasdlchan60@gmail.com>
2019-02-03 03:16:19 -0500

    Final Ui Touches
    graph changes

--- SoSChromeExtension/popup.js
+++ SoSChromeExtension/popup.js
@@ -15,23 +15,32 @@
 });*/
 
 
-let stockGrowthTen = function(price){
-  let arr = [];
-  let growPrice = 0;
-  arr.push(price)
-  for(var year = 1; year < 10; year++){
-    growPrice = arr[year-1] * 1.07;
-    arr.push(growPrice);
-  }
-  return arr;
-}
-let rawPrices = stockGrowthTen(50);
+// let stockGrowthTen = function(price){
+//   let arr = [];
+//   let growPrice = 0;
+//   arr.push(price)
+//   for(var year = 1; year < 10; year++){
+//     growPrice = arr[year-1] * 1.07;
+//     arr.push(growPrice);
+//   }
+//   return arr;
+// }
+function setChart(data){
+let rawPrices = data;
+let xLabels = []
+let count = 1;
+data.map(function(x){
+  xLabels.push(count);
+  count+=1;
+})
 console.log(rawPrices)
 var ctx = document.getElementById("compoundChart").getContext('2d');
+var scalemin = rawPrices[0] - rawPrices[0] / 2;
+var scaleMax = rawPrices[-1] + rawPrices * 1.5;
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: [1,2,3,4,5,6,7,8,9,10],
+      labels: xLabels,
         datasets: [{
             label: '$ You Can Make',
             data: rawPrices,
@@ -48,12 +57,23 @@ var myChart = new Chart(ctx, {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:false
+                    beginAtZero:true
                 }
-            }]
-        }
-    }
-});
+            }],
+            xAxes: [{
+              ticks: {
+                  beginAtZero:false,
+                  stepSize: 2
+              },
+  						display: true,
+  						scaleLabel: {
+  							display: true,
+  							labelString: 'Year'
+  						}
+        }]
+      }
+}});
+}
 
 // chrome.runtime.onMessage.addListener(function(request, sender) {
 //   message.innerText = sender.tab.url;
@@ -75,9 +95,16 @@ function retrieveData(payload){
     console.log(res);
     console.log(res.alternative)
     let oppcost = document.getElementById("oppcost-text")
-    let finalTextOppCost = "With this cash you could buy " + res.alternative.qty.toString() + " " + res.alternative.name + "!"
+    let stockText = document.getElementById("stock-text")
+    let investText = document.getElementById("invest-text")
+    let finalStockText = "With this cash you could buy " + res.stock.qty.toString() + " shares of " + res.stock.name + "!";
+    let finalInvestText = "You could invest in our mutual funds, they could be worth $" + res.mutual_fund.fv.toFixed(2).toString() + "!"
+    let finalTextOppCost = "With this cash you could buy " + res.alternative.qty.toString() + " " + res.alternative.name + "!";
     console.log(finalTextOppCost);
     oppcost.innerHTML = finalTextOppCost;
+    stockText.innerHTML = finalStockText;
+    investText.innerHTML = finalInvestText;
+    setChart(res.mutual_fund.graph)
   })
 }
 
